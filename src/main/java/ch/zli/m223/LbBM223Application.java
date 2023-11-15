c23787089e556143e88d4644ba4e7610e1da253b
noahlauper <noahlauper@gmail.com>
2023-11-15 15:53:04 +0100

    initialize
    Added Spring project to Repository

---
+++ src/main/java/ch/zli/m223/LbBM223Application.java
@@ -0,0 +1,13 @@
+package ch.zli.m223;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LbBM223Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LbBM223Application.class, args);
+    }
+
+}
---
+++ src/test/java/ch/zli/m223/LbBM223ApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.zli.m223;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LbBM223ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
